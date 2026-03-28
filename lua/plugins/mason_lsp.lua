e02864d571b9d8bc1ef2149aeaa07417057b2479
Javokhir <javokhir.coding@gmail.com>
2026-03-28 15:24:07 +0500

    installed all the necessary language servers

--- lua/plugins/mason_lsp.lua
+++ lua/plugins/mason_lsp.lua
@@ -1,7 +1,18 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-			ensure_installed = {"lua_ls", "ty"},
+			ensure_installed = {
+				"lua_ls",
+				"ty",
+				"clangd",
+				"curlylint",
+				"css-lsp",
+				"ruff",
+				"bash-language-server",
+				"tsserver",
+				"yaml-language-server",
+				"ini_ls",
+			},
 			automatic_installation = true,
 		},
     dependencies = {
